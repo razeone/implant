8debde20e3592a3eec985ebbca365ae6b30338fe
Jorge Alcaraz <jorgealberto.alcaraz@bbva.com>
2017-08-08 18:42:06 -0500

    feature/Show data access

--- src/main/java/com/beeva/app/Main.java
+++ src/main/java/com/beeva/app/Main.java
@@ -1,8 +1,14 @@
 package com.beeva.app;
 import static spark.Spark.*;
+import com.beeva.app.person.PersonDao;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        get("/person", (req, res) -> {
+            PersonDao dao = new PersonDao();
+            res.status(200);
+            res.type("application/json");
+            return PersonDao.dataToJson(dao.getAllPersons());
+        });
     }
 }
